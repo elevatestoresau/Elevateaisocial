91f379a547c5bc4f189a9fa5e28f249b44cd4694
elevatestoresau <elevate.storesau@gmail.com>
2026-01-15 05:15:55 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',
+    base: './', 
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
+      outDir: 'dist'
     }
   };
 });
